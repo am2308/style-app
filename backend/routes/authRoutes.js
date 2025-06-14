6b01d15df10351b6261debfe4ecf9d58a584a450
am2308 <b110020510@gmail.com>
2025-06-14 14:15:11 +0100

    Delete backend/routes/authRoutes.js

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, login } = require("../controllers/authController");
-const router = express.Router();
-
-router.post("/login", login);
-// Sign-Up Route
-router.post("/signup", signup);
-
-module.exports = router;
