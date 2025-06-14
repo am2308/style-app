9d9ac375a5f04437cd784853495afea3c284c346
am2308 <b110020510@gmail.com>
2025-06-14 14:15:19 +0100

    Delete backend/routes/imageRoutes.js

--- backend/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { upload, uploadImage } = require("../controllers/imageController");
-
-const router = express.Router();
-
-// Image Upload Route
-router.post("/upload", upload.array("images", 10), uploadImage);
-
-module.exports = router;
